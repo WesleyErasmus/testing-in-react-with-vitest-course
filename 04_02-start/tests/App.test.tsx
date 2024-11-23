5b87e9c8d26983e62cf3273f38aee615662496a3
Wesley Erasmus <wesley.erasmus119@gmail.com>
2024-11-23 10:51:02 +0200

    uploading and completed exercise 04_02-start

--- 04_02-start/tests/App.test.tsx
+++ 04_02-start/tests/App.test.tsx
@@ -1,56 +1,56 @@
-// import { describe, expect, it } from 'vitest';
-// import { fireEvent, render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
 
-// import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import { WrappedApp, App } from '../src/App';
+import { renderWithProviders } from './test-utils';
+import FruitSearch from '../src/components/FruitSearch';
 
-// import { WrappedApp, App } from '../src/App';
-// import { renderWithProviders } from './test-utils';
-// import FruitSearch from '../src/components/FruitSearch';
+describe('App', () => {
+  it('Renders hello world', () => {
+    // ARRANGE
+    renderWithProviders(<WrappedApp />);
 
-// describe('App', () => {
-//   it('Renders hello world', () => {
-//     // ARRANGE
-//     renderWithProviders(<WrappedApp />);
+    // ACT
 
-//     // ACT
+    // ASSERT
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Hello World');
+  });
 
-//     // ASSERT
-//     expect(
-//       screen.getByRole('heading', {
-//         level: 1,
-//       })
-//     ).toHaveTextContent('Hello World');
-//   });
+  it('Renders fruit search component and fetches fruit data', async () => {
+    renderWithProviders(<FruitSearch />);
 
-//   it('Renders fruit search component and fetches fruit data', async () => {
-//     renderWithProviders(<FruitSearch />);
+    const input = screen.getByPlaceholderText(/Search for a fruit/i);
+    fireEvent.change(input, { target: { value: 'pear' } });
 
-//     const input = screen.getByPlaceholderText(/Search for a fruit/i);
-//     fireEvent.change(input, { target: { value: 'pear' } });
+    // Wait for the async action to finish
+    const fruitTitle = await screen.findByText(/Fruit: pear/i);
 
-//     // Wait for the async action to finish
-//     const fruitTitle = await screen.findByText(/Fruit: pear/i);
+    expect(fruitTitle).toBeInTheDocument();
+  });
 
-//     expect(fruitTitle).toBeInTheDocument();
-//   });
+  it('Renders not found if path is invalid', () => {
+    // ARRANGE
+    render(
+      <MemoryRouter initialEntries={['/j']}>
+        <App />
+      </MemoryRouter>
+    );
 
-//   it('Renders not found if path is invalid', () => {
-//     // ARRANGE
-//     render(
-//       <MemoryRouter initialEntries={['/j']}>
-//         <App />
-//       </MemoryRouter>
-//     );
+    // ACT
 
-//     // ACT
-
-//     // ASSERT
-//     expect(
-//       screen.getByRole('heading', {
-//         level: 1,
-//       })
-//     ).toHaveTextContent(
-//       'Sorry the page you are looking for cannot be found, head Home and try again'
-//     );
-//   });
-// });
+    // ASSERT
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent(
+      'Sorry the page you are looking for cannot be found, head Home and try again'
+    );
+  });
+});
